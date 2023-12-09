ca4538cef8de6a6eb37bc604ab734c221913689b
Anamika Srivastava <78643538+ohmyanamika@users.noreply.github.com>
2023-12-10 04:42:11 +0530

    Update config.js

--- server/db/config.js
+++ server/db/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   'username': process.env.MYSQL_USER || 'root',
-  'password': process.env.MYSQL_PASSWORD || 'sona123',
+  'password': process.env.MYSQL_PASSWORD || 'root',
   'database': process.env.MYSQL_DATABASE || 'compoundDB',
   'host': process.env.MYSQL_HOST ||'localhost',
   'port': process.env.MYSQL_PORT || '3306',
